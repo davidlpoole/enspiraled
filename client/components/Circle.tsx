e2882f2bddcae5748f086654164295ea97106a41
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-05-07 10:59:55 +1200

    Add colours and recursive circles

--- client/components/Circle.tsx
+++ client/components/Circle.tsx
@@ -1,10 +1,51 @@
+import { useState } from 'react'
+
 interface Props {
   cx: number
   cy: number
   level: number
   r: number
+  fill: string
 }
 
-export default function Circle(props: Props) {
-  return <circle cx={props.cx} cy={props.cy} r={props.r} />
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+export default function Circle({ level, cx, cy, r }: Props) {
+  const [clicked, setClicked] = useState(false)
+
+  if (level > 7) return null
+
+  const handleClick = () => setClicked(true)
+
+  const generateCircle = (offsetX: number, offsetY: number) => (
+    <Circle
+      level={level + 1}
+      cx={cx + (offsetX * r) / 2}
+      cy={cy + (offsetY * r) / 2}
+      r={r / 2}
+      fill={colors[(level + 1) % colors.length]}
+    />
+  )
+
+  const circles = clicked && (
+    <>
+      {generateCircle(-1, 0)}
+      {generateCircle(0, -1)}
+      {generateCircle(1, 0)}
+      {generateCircle(0, 1)}
+    </>
+  )
+
+  return (
+    <>
+      <circle
+        onClick={handleClick}
+        cx={cx}
+        cy={cy}
+        r={r}
+        fill={colors[level % colors.length]}
+      />
+      {circles}
+    </>
+  )
 }
